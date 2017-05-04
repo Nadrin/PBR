cdb9161e4d5060093e84d89d8611ff86659ed079
Michał Siejak <michal@siejak.pl>
2017-05-04 03:12:40 +0200

    Add required third party libraries
    - Assimp
    - GLAD OpenGL loader for 4.5 core profile
    - GLFW
    - glm
    - stb_image
    
    Also included are win64 binaries built against MSVC2017 release multithreaded crt.

---
+++ lib/stb/src/libstb.c
@@ -0,0 +1,6 @@
+/* stb_image - v2.15 - public domain image loader - http://nothings.org/stb_image.h
+                                     no warranty implied; use at your own risk
+*/
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
