f2a7bf42d776a3e506454c41a92f9ad29accf5a1
Michał Siejak <michal@siejak.pl>
2017-12-03 18:35:30 +0100

    Added some misc utility functions & UTF8/UTF16 string conversions needed for Windows platform

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -5,6 +5,11 @@
 
 #include <fstream>
 #include <sstream>
+#include <memory>
+
+#if _WIN32
+#include <Windows.h>
+#endif // _WIN32
 
 #include "utils.hpp"
 
@@ -26,3 +31,21 @@ std::vector<char> File::readBinary(const std::string& filename)
 	file.read(buffer.data(), size);
 	return buffer;
 }
+
+#if _WIN32
+std::string Utility::convertToUTF8(const std::wstring& wstr)
+{
+	const int bufferSize = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	const std::unique_ptr<char[]> buffer(new char[bufferSize]);
+	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, buffer.get(), bufferSize, nullptr, nullptr);
+	return std::string(buffer.get());
+}
+
+std::wstring Utility::convertToUTF16(const std::string& str)
+{
+	const int bufferSize = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+	const std::unique_ptr<wchar_t[]> buffer(new wchar_t[bufferSize]);
+	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buffer.get(), bufferSize);
+	return std::wstring(buffer.get());
+}
+#endif // _WIN32
--- src/common/utils.hpp
+++ src/common/utils.hpp
@@ -14,3 +14,25 @@ public:
 	static std::string readText(const std::string& filename);
 	static std::vector<char> readBinary(const std::string& filename);
 };
+
+class Utility
+{
+public:
+	template<typename T> static constexpr T roundToPowerOfTwo(T value, int POT)
+	{
+		return (value + POT - 1) & -POT;
+	}
+	template<typename T> static constexpr T numMipmapLevels(T width, T height)
+	{
+		T levels = 1;
+		while((width|height) >> levels) {
+			++levels;
+		}
+		return levels;
+	}
+
+#if _WIN32
+	static std::string convertToUTF8(const std::wstring& wstr);
+	static std::wstring convertToUTF16(const std::string& str);
+#endif // _WIN32
+};
