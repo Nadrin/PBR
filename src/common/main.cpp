d279539d1ab5abbd91b6c448298ccfec3cafc6f1
Michał Siejak <michal@siejak.pl>
2018-07-24 08:07:31 +0200

    D3D11 renderer is now default on Windows

--- src/common/main.cpp
+++ src/common/main.cpp
@@ -11,35 +11,44 @@
 
 #include "../opengl.hpp"
 #include "../vulkan.hpp"
-#if _WIN32
+
+#if defined(PLATFORM_WINDOWS)
 #include "../d3d11.hpp"
 #include "../d3d12.hpp"
-#endif // _WIN32
+#endif
 
 int main(int argc, char* argv[])
 {
 	try {
 		RendererInterface* renderer;
-		if(argc < 2 || strcmp(argv[1], "-opengl") == 0) {
+
+		if(argc < 2) {
+#if defined(PLATFORM_WINDOWS)
+			renderer = new D3D11::Renderer;
+#else
+			renderer = new OpenGL::Renderer;
+#endif
+		}
+		else if(strcmp(argv[1], "-opengl") == 0) {
 			renderer = new OpenGL::Renderer;
 		}
 		else if(strcmp(argv[1], "-vulkan") == 0) {
 			renderer = new Vulkan::Renderer;
 		}
-#if _WIN32
+#if defined(PLATFORM_WINDOWS)
 		else if(strcmp(argv[1], "-d3d11") == 0) {
 			renderer = new D3D11::Renderer;
 		}
 		else if(strcmp(argv[1], "-d3d12") == 0) {
 			renderer = new D3D12::Renderer;
 		}
-#endif // _WIN32
+#endif
 		else {
-#if _WIN32
+#if defined(PLATFORM_WINDOWS)
 			std::fprintf(stderr, "Usage: %s [-opengl|-vulkan|-d3d11|-d3d12]\n", argv[0]);
 #else
 			std::fprintf(stderr, "Usage: %s [-opengl|-vulkan]\n", argv[0]);
-#endif // _WIN32
+#endif
 			return 1;
 		}
 
